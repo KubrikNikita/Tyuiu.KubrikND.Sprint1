d805a0381a62e32cb6733d627b4a1e7c0d31bd71
Никита Кубрик <kubriknd@outlook.com>
2023-10-11 20:47:05 +0500

    Task4(v29)

---
+++ Tyuiu.KubrikND.Sprint1.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KubrikND.Sprint1.Task4.V29.Lib;
+
+namespace Tyuiu.KubrikND.Sprint1.Task4.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double wait = 1 / 12;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KubrikND.Sprint1.Task4.V29/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KubrikND.Sprint1.Task4.V29.Lib;
+
+namespace Tyuiu.KubrikND.Sprint1.Task4.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Кубрик Н.Д.| ИСПБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант 29                                                               *");
+            Console.WriteLine("* Выполнил: Кубрик Н.Д.| ИСПБ-23-1                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на C#, которая запрашивает у пользователя *");
+            Console.WriteLine("*исходные данные и вычисляет результат по формуле                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x, y;
+            Console.WriteLine("Введите значение Х: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Результат:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("sqrt(2+|x-2y|)/3xy^2 =" + ds.Calculate(x, y));
+            Console.ReadLine();
+        }
+    }
+}
