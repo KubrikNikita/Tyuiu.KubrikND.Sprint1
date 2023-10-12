903e4f1b1942b3bade46f655b94e702775403bf4
Никита Кубрик <kubriknd@outlook.com>
2023-10-12 21:22:58 +0500

    Task7

---
+++ Tyuiu.KubrikND.Sprint1.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KubrikND.Sprint1.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KubrikND.Sprint1.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0c2da46-0557-444b-a0db-7a6ab710c149")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KubrikND.Sprint1.Task7.V0/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KubrikND.Sprint1.Task7.V0.Lib;
+
+namespace Tyuiu.KubrikND.Sprint1.Task7.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Кубрик Н.Д.| ИСПБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант 0                                                               *");
+            Console.WriteLine("* Выполнил: Кубрик Н.Д.| ИСПБ-23-1                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на C#, которая решает задачу по формуле   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x, y, z;
+            Console.WriteLine("Введите значение Х");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение У");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Z");
+            z = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Результат:                                                                 ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x, y, z));
+            Console.ReadKey();
+        }
+    }
+}
